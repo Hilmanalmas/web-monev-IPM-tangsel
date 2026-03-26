15ac8ccb3c0d08fbaddff7ee6d2bd79ecb047f10
Hilman (Imen) <141418226+Hilmanalmas@users.noreply.github.com>
2026-03-26 16:06:22 +0700

    feat: add initial database migrations for users, password reset tokens, and sessions tables.

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->enum('role', ['admin', 'observer', 'peserta'])->default('peserta');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('nip')->nullable();
+            $table->string('asal_instansi')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
